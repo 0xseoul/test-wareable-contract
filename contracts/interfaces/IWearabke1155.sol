89349cca6f616d561f2f3a587238b739e5c59e4d
abehan7 <hanjk13262@gmail.com>
2022-07-18 00:32:31 +0900

    add:> interface itemhandler

--- contracts/interfaces/IWearabke1155.sol
+++ contracts/interfaces/IWearabke1155.sol
@@ -12,20 +12,16 @@ interface IWearable1155 {
      * @notice This struct contains data related to a Staked Tokens
      *
      */
-
     /// @notice event emitted when a user has burned a ERC1155 nft
-    event BurnedCloths(address owner, uint256 tokenId);
-
+    // event BurnedCloths(address owner, uint256 tokenId);
     /// @notice event emitted when a user has minted a ERC1155 nft
-    event MintedCloths(address owner, uint256 tokenId);
-
-    function burnCloths(uint256 tokenId, uint256 erc1155Id)
-        external
-        payable
-        returns (bool success);
-
-    function mintCloths(uint256 tokenId, uint256 erc1155Id)
-        external
-        payable
-        returns (bool success);
+    // event MintedCloths(address owner, uint256 tokenId);
+    // function burnCloths(uint256 tokenId, uint256 erc1155Id)
+    //     external
+    //     payable
+    //     returns (bool success);
+    // function mintCloths(uint256 tokenId, uint256 erc1155Id)
+    //     external
+    //     payable
+    //     returns (bool success);
 }
