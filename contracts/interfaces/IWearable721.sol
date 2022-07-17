c8f76365899b3c443461ee6742835cfbe60048ca
abehan7 <hanjk13262@gmail.com>
2022-07-18 00:24:38 +0900

    feat:> IWearable event

---
+++ contracts/interfaces/IWearable721.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.15;
+
+/**
+ * @title IWearable721
+ * @author Abe
+ * @dev Wearable721 interface.
+ */
+
+interface IWearable721 {
+    /**
+     * @notice This struct contains data related to a Staked Tokens
+     *
+     * @param top - erc1155 token id of the top token
+     * @param bottom - erc1155 token id of the bottom token
+     */
+
+    struct TokenInfo {
+        uint256 top;
+        uint256 bottom;
+    }
+
+    /// @notice event emitted when a user has staked a nft
+    event Staked(address owner, uint256 tokenId);
+
+    /// @notice event emitted when a user has unstaked a nft
+    event Unstaked(address owner, uint256 tokenId);
+
+    /// @notice event emitted when a user has burned a ERC1155 nft
+    event BurnedCloths(address owner, uint256 tokenId);
+
+    /// @notice event emitted when a user has minted a ERC1155 nft
+    event MintedCloths(address owner, uint256 tokenId);
+}
